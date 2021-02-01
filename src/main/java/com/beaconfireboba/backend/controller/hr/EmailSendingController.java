af9c3eb107b698ea463a9ffbfc0bacc749ac5248
xuan-liu <lxuan95@163.com>
2021-02-01 02:35:34 -0500

    change email sending controller url

--- src/main/java/com/beaconfireboba/backend/controller/hr/EmailSendingController.java
+++ src/main/java/com/beaconfireboba/backend/controller/hr/EmailSendingController.java
@@ -1,23 +1,14 @@
 package com.beaconfireboba.backend.controller.hr;
 
-import com.beaconfireboba.backend.domain.common.ServiceStatus;
 import com.beaconfireboba.backend.domain.hr.HrEmailSendRequest;
-import com.beaconfireboba.backend.domain.hr.visaManagement.VisaManagementRequest;
-import com.beaconfireboba.backend.domain.hr.visaManagement.VisaManagementResponse;
-import com.beaconfireboba.backend.domain.hr.visaManagement.VisaManagementUploadRequest;
-import com.beaconfireboba.backend.entity.PersonalDocument;
-import com.beaconfireboba.backend.entity.VisaStatus;
-import com.beaconfireboba.backend.service.hr.VisaManagementService;
 import com.beaconfireboba.backend.util.MailUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
-@RequestMapping(value="/hr")
+@RequestMapping(value="/hr/sendEmail")
 public class EmailSendingController {
     private MailUtil mailUtil;
 
@@ -27,7 +18,7 @@ public class EmailSendingController {
     }
 
     @Async
-    @PostMapping(value="/sendEmail")
+    @PostMapping
     @ResponseBody
     public void sendEmailToEmployee(@RequestBody HrEmailSendRequest hrEmailSendRequest){
         mailUtil.sendGeneralMail(hrEmailSendRequest.getEmail(), hrEmailSendRequest.getSubject(),
